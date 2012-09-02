32eda5468e6d47f4b22c8363f4c1c1afe273014c
Nikola-K <nikola.kovacevic91@gmail.com>
2012-09-02 23:39:40 +0200

    Fixed error when indexing episodes that haven't aired yet and therefore don't have plot

--- imdbdb.py
+++ imdbdb.py
@@ -36,56 +36,56 @@ def create_db():
                  summary text, imdburl text)''')
     return 'Database created'
 
-def get_from_db(season,episode):
-    episode=str(episode)
-    season=str(season)
-    if season!='' and episode!='':#get by season,episode
+def get_from_db(season, episode):
+    if season != '' and episode != '':#get by season,episode
         try:
-            c.execute('select * from episodes where season=?', season)
+            c.execute('select * from episodes where season=?', str(season))
         except:
             return 'IMDbDB module error, no such season and or episode?'
         for row in c:
-            if row[1]==episode:
-                res=row
-                season=''
-                episode=''
-                title=''
-                plot=''
-                url=''
-                if int(row[0])<10:
-                    season='0'+row[0]
+            if row[1] == str(episode):
+                season = ''
+                episode = ''
+                title = ''
+                plot = ''
+                url = ''
+                if int(row[0]) < 10:
+                    season = '0' + row[0]
                 else:
-                    season=row[0]
-                if int(row[1])<10:
-                    episode='0'+row[1]
+                    season = row[0]
+                if int(row[1]) < 10:
+                    episode = '0' + row[1]
                 else:
-                    episode=row[1]
-                title=str(row[2])
-                plot=str(row[3]).replace('::garykmcd"]','').replace('[u"','').split('u"')[0]
-                url='http://www.imdb.com/title/tt'+str(row[4])
-                return 'S'+season+'E'+episode+':'+title+' : '+url+' | Plot: '+ plot
+                    episode = row[1]
+                title = str(row[2])
+                plot = str(row[3]).replace('::garykmcd"]', '').replace('[u"', '').split('u"')[0]
+                url = 'http://www.imdb.com/title/tt' + str(row[4])
+                return 'S' + season + 'E' + episode + ':' + title + ' : ' + url + ' | Plot: ' + plot
 
-def add_to_db(season,episode,ep_name,ep_plot,imdb_url):
-    t=(str(season),str(episode),str(ep_name),str(ep_plot),str(imdb_url))
-    c.execute('insert into episodes values (?,?,?,?,?)',t)
+def add_to_db(season, episode, ep_name, ep_plot, imdb_url):
+    t = (str(season), str(episode), str(ep_name), str(ep_plot), str(imdb_url))
+    c.execute('insert into episodes values (?,?,?,?,?)', t)
     conn.commit()#needs to be here?
 
 def update_db():
-    i = IMDb(accessSystem='mobile')
+    i = IMDb(accessSystem='mobile')# accessSystem='mobile' should use less bandwidth
     m = i.get_movie('0903747')
     i.update(m, 'episodes')
 
     print m['episodes']
     for season in m['episodes']:
         for ep in m['episodes'][season]:
-            print 'Downloading: '+'S:'+str(season)+'E:'+str(ep)
-            e=m['episodes'][season][ep]
+            print 'Downloading: ' + 'S:' + str(season) + 'E:' + str(ep)
+            e = m['episodes'][season][ep]
             i.update(e)
-            ep_name=e['title']
-            ep_plot=e['plot']
-            imdb_url=m['episodes'][season][ep].movieID
-            add_to_db(str(season),str(ep),str(ep_name),str(ep_plot),str(imdb_url))
-            print 'S'+str(season)+'E'+str(ep)+' Added to database'
+            ep_name = e['title']
+            try:
+                ep_plot = e['plot']
+            except:
+                ep_plot = 'N\A'
+            imdb_url = m['episodes'][season][ep].movieID
+            add_to_db(str(season), str(ep), str(ep_name), str(ep_plot), str(imdb_url))
+            print 'S' + str(season) + 'E' + str(ep) + ' Added to database'
     c.close()
 if __name__ == '__main__':
 #    create_db()
