319ebfa78c49d1f1d5cb192394785534864d5c98
Nikola-K <nikola.kovacevic91@gmail.com>
2012-09-02 19:46:21 +0200

    No major changes, now when urlib encounters error while trying to open goo.gl and shorten the link the error doesn't crash the bot

--- bblib.py
+++ bblib.py
@@ -19,6 +19,7 @@
 #       along with this program; if not, write to the Free Software
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA.
+#       
 
 from time import gmtime, strftime
 from datetime import datetime
@@ -26,17 +27,21 @@ import shelve
 import googl
 
 db = shelve.open('database.db')
-api_key = db['api_key']
-if api_key!='':
-    client=googl.Googl(api_key)
-admin=db['admin']
-password=db['password']
-banned=db['banned']
-admin=db['admin']
-auth=db['auth']
-initialch=db['initialch']
-cc=db['cc']
-episodes={'06.08.12:02:00:00':'Breaking Bad: Season 5, Episode 4 "Fifty-One" (5 Aug. 2012)',
+try:
+    api_key = db['api_key']
+    if api_key != '':
+        client = googl.Googl(api_key)
+        admin = db['admin']
+        password = db['password']
+        banned = db['banned']
+        admin = db['admin']
+        auth = db['auth']
+        initialch = db['initialch']
+        cc = db['cc']
+except KeyError:
+    print 'database error, run create_db.py to create new database.'
+    
+episodes = {'06.08.12:02:00:00':'Breaking Bad: Season 5, Episode 4 "Fifty-One" (5 Aug. 2012)',
           '13.08.12:02:00:00':'Breaking Bad: Season 5, Episode 5 "Dead Freight" (12 Aug. 2012)',
           '20.08.12:02:00:00':'Breaking Bad: Season 5, Episode 6 "Buyout" (19 Aug. 2012)',
           '27.08.12:02:00:00':'Breaking Bad: Season 5, Episode 7 "Say My Name" (26 Aug. 2012)',
@@ -44,18 +49,18 @@ episodes={'06.08.12:02:00:00':'Breaking Bad: Season 5, Episode 4 "Fifty-One" (5 
           '15.07.13:02:00:00':'Breaking Bad: Season 5, Episode 9 N/A - approximate date (Jul. 15, 2013)',
           }
 def getairtime():
-    tmpairtimes=[]
+    tmpairtimes = []
     for item in episodes:
         tmpairtimes.append(item)
     airtimes = [datetime.strptime(ts, "%d.%m.%y:%H:%M:%S") for ts in tmpairtimes]
     airtimes.sort()
     airtimes = [datetime.strftime(ts, "%d.%m.%y:%H:%M:%S") for ts in airtimes]
     t1 = strftime('%d.%m.%y:%H:%M:%S', gmtime())#current date/time GMT
-    ctime=datetime.strptime(t1,'%d.%m.%y:%H:%M:%S')
+    ctime = datetime.strptime(t1, '%d.%m.%y:%H:%M:%S')
     for t in airtimes:
         try:
-            airtime=datetime.strptime(t,'%d.%m.%y:%H:%M:%S')
-            if ctime<=airtime:
+            airtime = datetime.strptime(t, '%d.%m.%y:%H:%M:%S')
+            if ctime <= airtime:
                 return t
         except ValueError:
             return 'error'
@@ -63,8 +68,8 @@ def getairtime():
         return 'error'
 
 def getepisode():
-    airtime=getairtime()
-    if airtime=='error':
+    airtime = getairtime()
+    if airtime == 'error':
         return 'N/A'
     try:
         return episodes[airtime]
@@ -72,35 +77,35 @@ def getepisode():
         return 'N/A'
 
 def timediff():
-    airtime=getairtime()
-    if airtime=='error':
+    airtime = getairtime()
+    if airtime == 'error':
         return 'N/A'
     ctime = strftime('%d.%m.%y:%H:%M:%S', gmtime())
-    t1=datetime.strptime(ctime,'%d.%m.%y:%H:%M:%S')
-    t2=datetime.strptime(airtime,'%d.%m.%y:%H:%M:%S')
-    sec = t2-t1
-    time=str(sec)
-    ret=''
-    ssplit=1
-    day=time.split(',')[0].strip()
+    t1 = datetime.strptime(ctime, '%d.%m.%y:%H:%M:%S')
+    t2 = datetime.strptime(airtime, '%d.%m.%y:%H:%M:%S')
+    sec = t2 - t1
+    time = str(sec)
+    ret = ''
+    ssplit = 1
+    day = time.split(',')[0].strip()
     if day.split(' ')[0].isdigit():
-        ret+=day
-    
+        ret += day
+        
     try:
         time.split(',')[1].strip()
     except IndexError:
-        ssplit=0
-    hours=time.split(',')[ssplit].strip()
-    if hours.split(':')[0]!='00':
-        ret+=' '+hours.split(':')[0]+' hours'
+        ssplit = 0
+    hours = time.split(',')[ssplit].strip()
+    if hours.split(':')[0] != '00':
+        ret += ' ' + hours.split(':')[0] + ' hours'
     
-    minutes=time.split(',')[ssplit].strip()
-    if minutes.split(':')[1]!='00':
-        ret+=' '+minutes.split(':')[1]+' minutes'
+    minutes = time.split(',')[ssplit].strip()
+    if minutes.split(':')[1] != '00':
+        ret += ' ' + minutes.split(':')[1] + ' minutes'
 
-    seconds=time.split(',')[ssplit].strip()
-    if seconds.split(':')[2]!='00':
-        ret+=' '+seconds.split(':')[2]+' seconds'
+    seconds = time.split(',')[ssplit].strip()
+    if seconds.split(':')[2] != '00':
+        ret += ' ' + seconds.split(':')[2] + ' seconds'
     return ret
 
 def shorten(value):
@@ -111,110 +116,113 @@ def shorten(value):
         return str(e)
 
 
-def addremove(kind,what,value):
-    error='Error adding %s | Usage .<add/remove> <sstream/stream/download> <link>'
-    if what=='stream':
-        if value=='':
-            return error%('URL')
+def addremove(kind, what, value):
+    error = 'Error adding %s | Usage .<add/remove> <sstream/stream/download> <link>'
+    if what == 'stream':
+        if value == '':
+            return error % ('URL')
         try:
-            tmp=db['streamlinks']
-            if kind=='add':
+            tmp = db['streamlinks']
+            if kind == 'add':
                 tmp.append(value)
-                message='Link: '+value+' successfully added to stream list!'
-            if kind=='remove':
-                if value=='clear_all':
-                    tmp=['']
-                    message='Everything removed!'
+                message = 'Link: ' + value + ' successfully added to stream list!'
+            if kind == 'remove':
+                if value == 'clear_all':
+                    tmp = ['']
+                    message = 'Everything removed!'
                 else:
                     tmp.remove(value)
-                    message='Link: '+value+' successfully removed from stream list!'
-            if kind not in ['add','remove']:
-                return error%('stream')
-            db['streamlinks']=tmp
+                    message = 'Link: ' + value + ' successfully removed from stream list!'
+            if kind not in ['add', 'remove']:
+                return error % ('stream')
+            db['streamlinks'] = tmp
             db.sync()
             return message
         except ValueError:
-            return error%('or removing stream, database error.')
+            return error % ('or removing stream, database error.')
         
-    if what=='sstream':
-        if api_key=='':
+    if what == 'sstream':
+        if api_key == '':
             return 'goo.gl api key not entered'
-        if value=='':
-            return error%('URL')
+        if value == '':
+            return error % ('URL')
         try:
-            tmp=db['streamlinks']
-            if kind=='add':
-                if len(value.split())>1:
+            tmp = db['streamlinks']
+            if kind == 'add':
+                if len(value.split()) > 1:
                     print value.split()
                     return 'when using sstream only url is allowed, no addditional text'
-                svalue=shorten(value)
+                try:
+                    svalue = shorten(value)
+                except:
+                    return 'Error occured while trying to shorten link'
                 if not svalue.startswith('http://'):
-                    return 'Errror: '+svalue
+                    return 'Errror: ' + svalue
                 tmp.append(svalue)
-                message='Link: '+value+' successfully shortened to '+svalue+' and added to stream list!'
-            if kind=='remove':
-                message='Use .remove stream <shortened link> to remove it from the list.'
-            if kind not in ['add','remove']:
-                return error%('stream')
-            db['streamlinks']=tmp
+                message = 'Link: ' + value + ' successfully shortened to ' + svalue + ' and added to stream list!'
+            if kind == 'remove':
+                message = 'Use .remove stream <shortened link> to remove it from the list.'
+            if kind not in ['add', 'remove']:
+                return error % ('stream')
+            db['streamlinks'] = tmp
             db.sync()
             return message
         except ValueError:
-            return error%('or removing stream, database error.')
+            return error % ('or removing stream, database error.')
 
-    if what=='download':
-        if value=='':
-            return error%('download URL')
+    if what == 'download':
+        if value == '':
+            return error % ('download URL')
         try:
-            tmp=db['downloadlinks']
-            if kind=='add':
+            tmp = db['downloadlinks']
+            if kind == 'add':
                 tmp.append(value)
-                message='Link: '+value+' successfully added to download list!'
-            if kind=='remove':
+                message = 'Link: ' + value + ' successfully added to download list!'
+            if kind == 'remove':
                 tmp.remove(value)
-                message='Link: '+value+' successfully removed from download list!'
-            if kind not in ['add','remove']:
-                return error%('download link')
-            db['downloadlinks']=tmp
+                message = 'Link: ' + value + ' successfully removed from download list!'
+            if kind not in ['add', 'remove']:
+                return error % ('download link')
+            db['downloadlinks'] = tmp
             db.sync()
             return message
         except ValueError:
-            return error%('or removing stream, database error.')
-    if what not in ['stream','download']:
-        return error%('or removing links.')
+            return error % ('or removing stream, database error.')
+    if what not in ['stream', 'download', 'sstream']:
+        return error % ('or removing links.')
         
 
 def info():
     try:
-        reddit=db['reddit']
+        reddit = db['reddit']
     except ValueError:
-        reddit='N/A'
-    return 'Next episode: '+getepisode()+' | Airs in '+timediff()+' | Latest reddit discussion thread '+reddit
+        reddit = 'N/A'
+    return 'Next episode: ' + getepisode() + ' | Airs in ' + timediff() + ' | Latest reddit discussion thread ' + reddit
 
 def updatethread(url):
-    db['reddit']=url
+    db['reddit'] = url
     db.sync()
-    return 'New reddit lnk/thread: '+url
+    return 'New reddit lnk/thread: ' + url
     
 def download():
     try:
-        links=db['downloadlinks']
+        links = db['downloadlinks']
     except KeyError:
         return 'error'
-    string='%s' % ' '.join(map(str, links))
-    if string.strip()=='':
+    string = '%s' % ' '.join(map(str, links))
+    if string.strip() == '':
         return 'There are no download links to display currently!'
-    return 'Download links: '+string
+    return 'Download links: ' + string
 
 def stream():
     try:
-        links=db['streamlinks']
+        links = db['streamlinks']
     except KeyError:
         return 'error'
-    string='%s' % ' '.join(map(str, links))
-    if string.strip()=='':
+    string = '%s' % ' '.join(map(str, links))
+    if string.strip() == '':
         return 'There are no stream links to display currently! '
-    return 'Stream links: '+string
+    return 'Stream links: ' + string
 
 #if __name__ == '__main__':
 #    pass
